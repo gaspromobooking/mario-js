646e99fb80dd74b9623450ea4be83d8009840241
gaspromobooking <62516451+gaspromobooking@users.noreply.github.com>
2020-03-22 21:41:59 +0300

    2

--- mario.js
+++ mario.js
@@ -1,20 +1,19 @@
 
-printPyramid(18);
+printPyramid(5);
 
 
 /*
- * printPyramid
- *
- * Prints to the console a pyramid of '#' characters of the specified height
- * For example, if height is 5, the console will look like this:
- *          ##
- *         ###
- *        ####
- *       #####
- *      ######
- */
+
+
 function printPyramid(height) {
         for (let i = 1; i <= height; i+=1) {
         console.log(" ".repeat(height-i)+"#".repeat(i+1));
+        }
+}
+ */
+function printPyramid(height) {
+        for (let i = 1; i <= height; i+=1) {
+                child = document.createElement("p", " ".repeat(height-i)+"#".repeat(i+1))
+                document.getElementById('pyramid').appendChild(child)
         }
 }
